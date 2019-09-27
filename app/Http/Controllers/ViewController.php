416651a8f1ae96d1174506baa4c61c50f74f974c
Ben <alanbenedictgolpeo@gmail.com>
2019-09-27 17:33:42 +0800

    removed filter

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Request;
-use App\UserTrades;
 use App\UserHistory;
+use App\UserTrades;
+use Illuminate\Support\Facades\DB;
 
 class ViewController extends Controller
 {
@@ -21,7 +22,9 @@ class ViewController extends Controller
     	$keyword = Request::get('currency');
 
     	if(empty($keyword)){
-    		$default = UserHistory::with('user_sender','user_receiver')->where('currency_trade','btc')->orWhere('currency_request','btc')->get();
+    		/*$default = UserHistory::with('user_sender','user_receiver')->where('currency_trade','btc')->orWhere('currency_request','btc')->get();*/
+
+            $default = UserHistory::with('user_sender','user_receiver')->get();
 
     		return response()->json(['results' => $default]);
     	}
