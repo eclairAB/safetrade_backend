bf057620a35b3e20ae15f7b2265a3e1e256b3199
jeffreyson delejencia <jeffxion03@gmail.com>
2019-10-04 13:02:46 +0800

    Tradebot

---
+++ app/Console/Commands/AcceptTradebot.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\UserCurrency;
+use App\UserHistory;
+use App\UserTrades;
+use App\User;
+
+class AcceptTradebot extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'accept:trade';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Accept the trade bot.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+
+    // TRADE ROUTINE;
+    // user4 -> user5
+    // user5 -> user6
+    // user6 -> user7
+    // user7 -> user3
+
+    public function bot_3_to_4()
+    {
+        $list_trade_bot = UserTrades::where('user_id',3)->where('status',1)->first();
+        $bot_4_wallet = UserCurrency::where('user_id',4)->first();
+        $bot_3_wallet = UserCurrency::where('user_id',3)->first();
+
+        if($list_trade_bot != null){
+            $value = $list_trade_bot->trade_currency;
+            if($bot_3_wallet->$value >= $list_trade_bot->trade_amount){
+                $history =  new UserHistory;
+
+                $history->sender_id = 3;
+                $history->receiver_id = 4;
+                $history->amount = $list_trade_bot->trade_amount;
+                $history->transaction_option = "trade";
+                $history->currency_trade = $list_trade_bot->trade_currency;
+                $history->currency_request = $list_trade_bot->request_currency;
+
+                $bot_3_wallet->decrement($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+                $bot_3_wallet->increment($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+
+                $bot_4_wallet->decrement($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+                $bot_4_wallet->increment($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+
+                if($history->save()){
+                    $list_trade_bot->status = 0;
+                    $list_trade_bot->save();
+                    $this->line('Sucess! bot3');
+                }
+            }else{
+                $this->line('bot3 is out of balance!');
+            }
+        }else{
+            $this->line('bot3 has no trade post');
+        }
+    }
+
+    public function bot_4_to_5()
+    {
+        $list_trade_bot = UserTrades::where('user_id',4)->where('status',1)->first();
+        $bot_5_wallet = UserCurrency::where('user_id',5)->first();
+        $bot_4_wallet = UserCurrency::where('user_id',4)->first();
+        
+
+        if($list_trade_bot != null){
+            $value = $list_trade_bot->trade_currency;
+            if($bot_4_wallet->$value >= $list_trade_bot->trade_amount){
+                $history =  new UserHistory;
+
+                $history->sender_id = 4;
+                $history->receiver_id = 5;
+                $history->amount = $list_trade_bot->trade_amount;
+                $history->transaction_option = "trade";
+                $history->currency_trade = $list_trade_bot->trade_currency;
+                $history->currency_request = $list_trade_bot->request_currency;
+
+                $bot_4_wallet->decrement($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+                $bot_4_wallet->increment($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+
+                $bot_5_wallet->decrement($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+                $bot_5_wallet->increment($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+
+                if($history->save()){
+                    $list_trade_bot->status = 0;
+                    $list_trade_bot->save();
+                    $this->line('Sucess! bot4');
+                }
+            }else{
+                $this->line('bot4 is out of balance!');
+            }
+        }else{
+            $this->line('bot4 has no trade post');
+        }
+    }
+
+    public function bot_5_to_6()
+    {
+        $list_trade_bot = UserTrades::where('user_id',5)->where('status',1)->first();
+        $bot_6_wallet = UserCurrency::where('user_id',6)->first();
+        $bot_5_wallet = UserCurrency::where('user_id',5)->first();
+        
+
+        if($list_trade_bot != null){
+            $value = $list_trade_bot->trade_currency;
+            if($bot_5_wallet->$value >= $list_trade_bot->trade_amount){
+                $history =  new UserHistory;
+
+                $history->sender_id = 5;
+                $history->receiver_id = 6;
+                $history->amount = $list_trade_bot->trade_amount;
+                $history->transaction_option = "trade";
+                $history->currency_trade = $list_trade_bot->trade_currency;
+                $history->currency_request = $list_trade_bot->request_currency;
+
+                $bot_5_wallet->decrement($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+                $bot_5_wallet->increment($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+
+                $bot_6_wallet->decrement($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+                $bot_6_wallet->increment($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+
+                if($history->save()){
+                    $list_trade_bot->status = 0;
+                    $list_trade_bot->save();
+                    $this->line('Sucess! bot5');
+                }
+            }else{
+                $this->line('bot5 is out of balance!');
+            }
+        }else{
+            $this->line('bot5 has no trade post');
+        }
+    }
+
+    public function bot_6_to_7()
+    {
+        $list_trade_bot = UserTrades::where('user_id',6)->where('status',1)->first();
+        $bot_7_wallet = UserCurrency::where('user_id',7)->first();
+        $bot_6_wallet = UserCurrency::where('user_id',6)->first();
+        
+
+        if($list_trade_bot != null){
+            $value = $list_trade_bot->trade_currency;
+            if($bot_6_wallet->$value >= $list_trade_bot->trade_amount){
+                $history =  new UserHistory;
+
+                $history->sender_id = 6;
+                $history->receiver_id = 7;
+                $history->amount = $list_trade_bot->trade_amount;
+                $history->transaction_option = "trade";
+                $history->currency_trade = $list_trade_bot->trade_currency;
+                $history->currency_request = $list_trade_bot->request_currency;
+
+                $bot_6_wallet->decrement($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+                $bot_6_wallet->increment($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+
+                $bot_7_wallet->decrement($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+                $bot_7_wallet->increment($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+
+                if($history->save()){
+                    $list_trade_bot->status = 0;
+                    $list_trade_bot->save();
+                    $this->line('Sucess! bot6');
+                }
+            }else{
+                $this->line('bot6 is out of balance!');
+            }
+        }else{
+            $this->line('bot6 has no trade post');
+        }
+    }
+
+    public function bot_7_to_3()
+    {
+        $list_trade_bot = UserTrades::where('user_id',7)->where('status',1)->first();
+        $bot_3_wallet = UserCurrency::where('user_id',3)->first();
+        $bot_7_wallet = UserCurrency::where('user_id',7)->first();
+        
+
+        if($list_trade_bot != null){
+            $value = $list_trade_bot->trade_currency;
+            if($bot_7_wallet->$value >= $list_trade_bot->trade_amount){
+                $history =  new UserHistory;
+
+                $history->sender_id = 7;
+                $history->receiver_id = 3;
+                $history->amount = $list_trade_bot->trade_amount;
+                $history->transaction_option = "trade";
+                $history->currency_trade = $list_trade_bot->trade_currency;
+                $history->currency_request = $list_trade_bot->request_currency;
+
+                $bot_7_wallet->decrement($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+                $bot_7_wallet->increment($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+
+                $bot_3_wallet->decrement($list_trade_bot->request_currency,$list_trade_bot->request_amount);
+                $bot_3_wallet->increment($list_trade_bot->trade_currency,$list_trade_bot->trade_amount);
+
+                if($history->save()){
+                    $list_trade_bot->status = 0;
+                    $list_trade_bot->save();
+                    $this->line('Sucess! bot7');
+                }
+            }else{
+                $this->line('bot7 is out of balance!');
+            }
+        }else{
+            $this->line('bot7 has no trade post');
+        }
+    }
+
+    public function handle()
+    {
+        
+        $list_trade_bots = UserTrades::whereIn('user_id',[3,4,5,6,7])->get();
+        foreach ($list_trade_bots as $list_trade_bot) {
+            switch ($list_trade_bot->user_id) {
+                case 3:
+                    echo $this->bot_3_to_4();
+                    break;
+                case 4:
+                    echo $this->bot_4_to_5();
+                    break;
+                case 5:
+                    echo $this->bot_5_to_6();
+                    break;
+                case 6:
+                    echo $this->bot_6_to_7();
+                    break;
+                case 7:
+                    echo $this->bot_7_to_3();
+                    break;
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/TradeAutoBot.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\UserCurrency;
+use App\UserTrades;
+use App\User;
+
+class TradeAutoBot extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'trade:bot';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Activate the autobot in trade post.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+
+
+    public function bot_validator()
+    {
+        foreach ($bot_wallets as $bot_wallet) {
+
+            $wallet = $second_wallets[$second_index];
+            $trades = new UserTrades;
+
+            foreach (range(1, $bot_wallet->$wallet) as $number) {
+                $possible_trade_amount = array(number_format($number,10));
+
+                $test_index = array_rand($possible_trade_amount);
+
+                $the_chosen_one = $possible_trade_amount[$test_index];
+                if($the_chosen_one > $bot_wallet->$wallet){
+                    echo "kulang ang balance". "\n";
+                }else{
+                    echo "okay ang balance". "\n";
+                    $trades->user_id = $bot_wallet->user_id;
+                    $trades->request_amount = $the_chosen_one;
+                    $trades->trade_amount = $the_chosen_one;
+                    $trades->request_currency = $first_wallets[$first_index];
+                    $trades->trade_currency = $second_wallets[$second_index];
+
+                    $trades->save();
+
+                    $this->line("done");
+                }
+            }
+        }
+    }
+
+
+
+    public function handle()
+    {
+        $this->line('Getting started...');
+        $minimum_amount = 1;
+
+        $first_wallets = array("btc", "eth","xrp", "ltc", "bch");
+        $second_wallets = array("eos", "bnb", "usdt", "bsv", "trx");
+
+        $first_index = array_rand($first_wallets);
+        $second_index = array_rand($second_wallets);
+
+        $bot_wallets = UserCurrency::whereIn('user_id', [3,4,5,6,7])->get();
+        $bot_active_trade = UserTrades::whereIn('user_id', [3,4,5,6,7])->where('status',1)->get();
+
+
+        if($bot_active_trade->count() > 0){
+            return $this->bot_validator();
+        }else{
+            
+            foreach ($bot_wallets as $bot_wallet) {
+
+                $wallet = $second_wallets[$second_index];
+                $trades = new UserTrades;
+
+                foreach (range(1, $bot_wallet->$wallet) as $number) {
+                    $possible_trade_amount = array(number_format($number,10));
+
+                    $test_index = array_rand($possible_trade_amount);
+
+                    $the_chosen_one = $possible_trade_amount[$test_index];
+                    if($the_chosen_one > $bot_wallet->$wallet){
+                        echo "kulang ang balance". "\n";
+                    }else{
+                        echo "okay ang balance". "\n";
+                        $trades->user_id = $bot_wallet->user_id;
+                        $trades->request_amount = $the_chosen_one;
+                        $trades->trade_amount = $the_chosen_one;
+                        $trades->request_currency = $first_wallets[$first_index];
+                        $trades->trade_currency = $second_wallets[$second_index];
+
+                        $trades->save();
+
+                        $this->line("done");
+                    }
+                }
+            }
+
+        }
+    }
+}
