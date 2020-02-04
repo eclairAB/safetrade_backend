5d69b29d4d34e80242f42a1826b7627ea0784c0f
jeffrey Son <jeffxion03@gmail.com>
2020-02-04 09:33:26 +0800

    new bot ready to test

--- app/BotTrade.php
+++ app/BotTrade.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BotTrade extends Model
 {
-    //
+    protected $table = 'bot_trades';
+    protected $fillable = [
+        'wallet_one',
+		'wallet_two',
+		'min_one',
+		'max_one',
+		'min_two',
+		'max_two'
+	];
 }
